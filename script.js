fff8a77ccef160b6199aa475c8a31df147f882ee
Jeremy Yon <jeremyyon87@gmail.com>
2020-12-21 11:14:52 -0600

    Added confirms

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Assignment Code (Starter Code - Thanks!)
 var generateBtn = document.querySelector("#generate");
 
-//Arrays made to set out available characters
+//Arrays made to set out available characters//
 var themNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var themSpecchar = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "+", "="];
 var themLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var themUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-//Variables have to be added for confirmations
+//Variables have to be added for confirmations//
 var PasswordLength;
 var UppercaseConfirm;
 var LowercaseConfirm;
@@ -15,18 +15,42 @@ var SpecialcharConfirm;
 var NumberConfirm;
 
 
-//Javascript(JS) prompts about password length.
+//Javascript(JS) prompts about password length.//
 function BuildPassword(){
-  PasswordLength = prompt ("Tell me how long you want your password to be (Sidenote: It has to be between 8 to 128 characters long.)");
+  PasswordLength = prompt ("Tell me how long you want your password to be (Sidenote: It has to be between 8 to 128 characters long)!");
 
-//Conditions that verify that the length of inputted password is between 8 to 128 characters. If it is not, JS will send an alert to the user & put them back on the prompt screen.
+//Condition that verifies that the length of password is between 8 to 128 characters. If not, JS will send an alert to the user & redirect them back to the prompt screen.
 if (PasswordLength < 8 || PasswordLength > 128) {
   alert ("Password has to be 8 to 128 characters!");
   BuildPassword();
 }
 
-
-
+return PasswordLength;
 }
-// Add event listener to generate button
+
+//Questions if user wants uppercase letters in password//
+function RequestUppercase() {
+  UppercaseConfirm = confirm("You want uppercase letters in your password?");
+  return UppercaseConfirm;
+}
+
+//Questions if user wants uppercase letters in password//
+function RequestLowercase() {
+  LowercaseConfirm = confirm("You want lowercase letters in your password?");
+  return LowercaseConfirm;
+}
+
+//Questions if user wants special characters in password//
+function RequestSpecialchar() {
+  SpecialcharConfirm = confirm("You want special characters in your password?");
+  return SpecialcharConfirm;
+}
+
+//Questions if user wants numbers in password//
+function RequestNumbers() {
+  NumberConfirm = confirm("Do you want numbers in your password?");
+  return NumberConfirm;
+}
+
+// Add event listener to generate button //
 generateBtn.addEventListener("click", writePassword);
