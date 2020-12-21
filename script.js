b9547d093c308ce1dbecd1136af61844c6b3fd91
Jeremy Yon <jeremyyon87@gmail.com>
2020-12-21 09:46:20 -0600

    Adding arrays & variables

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// Assignment Code
+// Assignment Code (Starter Code - Thanks!)
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Arrays made to set out available characters
+var themNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var themSpecchar = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "+", "="];
+var themLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var themUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  passwordText.value = password;
+//Variables have to be added for confirmations
+var PasswordLength;
+var UppercaseConfirm;
+var LowercaseConfirm;
+var SpecialcharConfirm;
+var NumberConfirm;
 
-}
-function generatePassword() {
-  var charactercount = prompt("How many characters for password?");
-  if (charactercount < 8 || charactercount > 128) {
-    alert("You messed up!")
-    return "error"
-  }
 
 }
 // Add event listener to generate button
