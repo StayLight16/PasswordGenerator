227be86544e23fc1a835c410aafe887e95e051c5
Jeremy Yon <jeremyyon87@gmail.com>
2020-12-21 13:31:56 -0600

    just changes

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 //Arrays made to set out available characters//
-var themNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var themSpecchar = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "+", "="];
-var themLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var themUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var ThemNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var ThemSpecchar = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "+", "="];
+var ThemLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var ThemUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //Variables have to be added for confirmations//
 var PasswordLength;
@@ -16,10 +16,12 @@ var NumberConfirm;
 
 
 //Javascript(JS) prompts about password length.//
+
 function BuildPassword(){
   PasswordLength = prompt ("Tell me how long you want your password to be (Sidenote: It has to be between 8 to 128 characters long)!");
 
 //Condition that verifies that the length of password is between 8 to 128 characters. If not, JS will send an alert to the user & redirect them back to the prompt screen.
+
 if (PasswordLength < 8 || PasswordLength > 128) {
   alert ("Password has to be 8 to 128 characters!");
   BuildPassword();
@@ -45,6 +47,31 @@ function RequestSpecialchar() {
   SpecialcharConfirm = confirm("You want special characters in your password?");
   return SpecialcharConfirm;
 }
+
+// If proved to be true, arrays are subsequently mixed.//
+
+function CreatePassword() {
+
+  var RunCharacters = []
+  
+  if (UppercaseConfirm) {
+    RunCharacters = RunCharacters.concat(ThemUpperCase)
+  }
+  
+  if (LowercaseConfirm) {
+    RunCharacters = RunCharacters.concat(ThemLowercase)
+  }
+  
+  if (NumberConfirm) {
+    RunCharacters = RunCharacters.concat(ThemNumbers)
+  }
+  
+  if (SpecialcharConfirm) {
+    RunCharacters = RunCharacters.concat(ThemSpecchar)
+  }
+  
+  console.log(RunCharacters);
+
 
 //Questions if user wants numbers in password//
 function RequestNumbers() {
