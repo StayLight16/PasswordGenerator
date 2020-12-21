2d0849d970f1445f9b645dfe8eafd896e55c422f
Jeremy Yon <jeremyyon87@gmail.com>
2020-12-21 01:16:09 -0600

    added 1st validator

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  var charactercount = prompt("How many characters for password?");
+  if (charactercount < 8 || charactercount > 128) {
+    alert("You messed up!")
+    return "error"
+  }
 
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
